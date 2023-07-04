b3f4f2c7dfe31cee973ecb82eee783f73f21d21b
SeungWoo <wjknn3123@gmail.com>
2023-07-04 21:17:30 +0900

    Add Theme

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import {BrowserRouter} from "react-router-dom";
+import {QueryClient, QueryClientProvider} from "react-query";
+import {ReactQueryDevtools} from "react-query/devtools";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+    document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools initialIsOpen={true}/>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </QueryClientProvider>
+    </React.StrictMode>
 );
---
+++ src/styles/theme/font.ts
@@ -0,0 +1,59 @@
+export const Fonts = {
+    Heading1: {
+        weight: 700,
+        size: "40px",
+        height: 60
+    },
+    Heading2: {
+        weight: 700,
+        size: "36px",
+        height: 54
+    },
+    Heading3: {
+        weight: 700,
+        size: "32px",
+        height: 48
+    },
+    Heading4: {
+        weight: 500,
+        size: "28px",
+        height: 40
+    },
+    Heading5: {
+        weight: 500,
+        size: "24px",
+        height: 36
+    },
+    Heading6: {
+        weight: 500,
+        size: "20px",
+        height: 28
+    },
+    Body1: {
+        weight: 500,
+        size: "16px",
+        height: 24
+    },
+    Body2: {
+        weight: 400,
+        size: "16px",
+        height: 24
+    },
+    Body3: {
+        weight: 500,
+        size: "14px",
+        height: 20
+    },
+    Body4: {
+        weight: 400,
+        size: "14px",
+        height: 20
+    },
+    Caption: {
+        weight: 400,
+        size: "12px",
+        height: 18
+    }
+}
+
+export type fontsKeyOfType = keyof typeof Fonts
